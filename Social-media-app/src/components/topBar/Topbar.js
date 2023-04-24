e944c9ca4bd981ff932b86c5ac85d707b01b5869
thehanir95 <thehanicis95@gmail.com>
2023-04-24 17:22:17 +0530

    changes

---
+++ Social-media-app/src/components/topBar/Topbar.js
@@ -0,0 +1,9 @@
+import "./topbar.css"
+
+export default function Topbar() {
+    return (
+        <div>
+            toopbar
+        </div>
+    )
+}
---
+++ Social-media-app/src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import Topbar from "../../components/topBar/Topbar"
+
+export default function Home() {
+    return (
+        <Topbar/>
+    )
+}
